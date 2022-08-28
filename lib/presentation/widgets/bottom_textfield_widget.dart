2a6f014935e24c592866fa2aa8ba1ff2706cc31c
Javoh29 <33982593+Javoh29@users.noreply.github.com>
2022-08-28 22:46:52 +0500

    small changes

--- lib/presentation/widgets/bottom_textfield_widget.dart
+++ lib/presentation/widgets/bottom_textfield_widget.dart
@@ -16,11 +16,12 @@ class BottomTextFiledWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Positioned(
-      bottom: 20,
+      bottom: 0,
       left: 0,
       right: 0,
       child: Container(
         color: Colors.white,
+        padding: const EdgeInsets.only(bottom: 20, top: 10),
         child: ListTile(
           contentPadding: EdgeInsets.zero,
           leading: mLeading,
@@ -36,8 +37,9 @@ class BottomTextFiledWidget extends StatelessWidget {
               style: AppTextStyles.h5,
               decoration: InputDecoration(
                 border: InputBorder.none,
-                hintText: 'Ok great, Let me ask arou|',
-                hintStyle: AppTextStyles.h5,
+                hintText: 'Напишите комментарий',
+                hintStyle: AppTextStyles.h5.copyWith(color: AppColors.metalColor.shade50),
+                contentPadding: const EdgeInsets.symmetric(horizontal: 15),
               ),
             ),
           ),
