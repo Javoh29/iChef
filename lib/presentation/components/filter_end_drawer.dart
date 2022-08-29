4ef0a68687d096f4a48abad2d2c34e42f2031561
Umidjon Yoqubov <umidjonxomidjonovich@gmail.com>
2022-08-29 17:42:20 +0500

    added filter drawer

---
+++ lib/presentation/components/filter_end_drawer.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class FilterEndDrawer extends StatefulWidget {
+  const FilterEndDrawer({Key? key}) : super(key: key);
+
+  @override
+  State<FilterEndDrawer> createState() => _FilterEndDrawerState();
+}
+
+class _FilterEndDrawerState extends State<FilterEndDrawer> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
