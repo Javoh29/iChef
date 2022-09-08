cd17db642e39c8dbdb443703237526b365e737f6
Javoh29 <33982593+Javoh29@users.noreply.github.com>
2022-09-08 22:08:43 +0500

    update

--- lib/presentation/pages/shopping_list/components/item_details.dart
+++ lib/presentation/pages/shopping_list/components/item_details.dart
@@ -16,8 +16,7 @@ import '../../../../config/constants/assets.dart';
 import '../../../widgets/dismissible_panel_widget.dart';
 
 class ShoppingItemDetails extends StatefulWidget {
-  const ShoppingItemDetails({Key? key, required this.shippingList})
-      : super(key: key);
+  const ShoppingItemDetails({Key? key, required this.shippingList}) : super(key: key);
   final List<ShippingModel> shippingList;
 
   @override
@@ -58,9 +57,7 @@ class _ShoppingItemDetailsState extends State<ShoppingItemDetails> {
                         modelList.add(model);
                       } else {
                         model = modelList.removeAt(index);
-                        modelList.insert(
-                            model.id <= index ? model.id : modelList.length - 1,
-                            model);
+                        modelList.insert(model.id <= index ? model.id : modelList.length - 1, model);
                       }
                     });
                   },
@@ -107,8 +104,7 @@ class _ShoppingItemDetailsState extends State<ShoppingItemDetails> {
                       icon: Assets.icons.edit,
                       onTap: () {
                         setState(() {
-                          modelList[index].isShowEdit =
-                              !modelList[index].isShowEdit;
+                          modelList[index].isShowEdit = !modelList[index].isShowEdit;
                         });
                       },
                     ),
@@ -136,8 +132,7 @@ class _ShoppingItemDetailsState extends State<ShoppingItemDetails> {
 }
 
 class ItemWidget extends StatefulWidget {
-  const ItemWidget({Key? key, required this.model, required this.showEdit})
-      : super(key: key);
+  const ItemWidget({Key? key, required this.model, required this.showEdit}) : super(key: key);
   final ShippingModel model;
   final bool showEdit;
 
@@ -160,9 +155,8 @@ class _ItemWidgetState extends State<ItemWidget> {
 
   @override
   Widget build(BuildContext context) {
-    decoration = widget.model.isCheck
-        ? AppDecorations.defDecor.copyWith(color: AppColors.deletedItem)
-        : AppDecorations.defDecor;
+    decoration =
+        widget.model.isCheck ? AppDecorations.defDecor.copyWith(color: AppColors.deletedItem) : AppDecorations.defDecor;
 
     return Container(
       alignment: Alignment.center,
@@ -177,22 +171,16 @@ class _ItemWidgetState extends State<ItemWidget> {
               TextButton(
                 onPressed: () {},
                 style: AppDecorations.buttonStyle(
-                  bgColor: widget.model.isCheck
-                      ? AppColors.deletedItem
-                      : AppColors.primaryLight.shade50,
+                  bgColor: widget.model.isCheck ? AppColors.deletedItem : AppColors.primaryLight.shade50,
                   border: BorderSide(
                     width: 1,
-                    color: widget.model.isCheck
-                        ? AppColors.deletedItemBorder
-                        : AppColors.primaryLight.shade100,
+                    color: widget.model.isCheck ? AppColors.deletedItemBorder : AppColors.primaryLight.shade100,
                   ),
                 ),
                 child: Text(
                   widget.model.title,
                   style: AppTextStyles.b4Medium.copyWith(
-                    color: widget.model.isCheck
-                        ? AppColors.deletedItemBorder
-                        : AppColors.primaryLight.shade100,
+                    color: widget.model.isCheck ? AppColors.deletedItemBorder : AppColors.primaryLight.shade100,
                   ),
                 ),
               ),
@@ -201,8 +189,7 @@ class _ItemWidgetState extends State<ItemWidget> {
                 crossAxisAlignment: CrossAxisAlignment.end,
                 children: [
                   Text(widget.model.weight ?? '',
-                      style: AppTextStyles.b4DemiBold
-                          .copyWith(fontWeight: FontWeight.w700)),
+                      style: AppTextStyles.b4DemiBold.copyWith(fontWeight: FontWeight.w700)),
                   Row(
                     children: [
                       widget.model.subWeight != null
@@ -222,16 +209,12 @@ class _ItemWidgetState extends State<ItemWidget> {
                   ? Container(
                       height: 28,
                       margin: const EdgeInsets.only(left: 12),
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 14, vertical: 3),
-                      decoration: BoxDecoration(
-                          color: const Color(0xffE5E7EB),
-                          borderRadius: BorderRadius.circular(6)),
+                      padding: const EdgeInsets.symmetric(horizontal: 14, vertical: 3),
+                      decoration: BoxDecoration(color: const Color(0xffE5E7EB), borderRadius: BorderRadius.circular(6)),
                       child: Row(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          btnIncOrDec(
-                              icon: Assets.icons.removeBlack, isInc: false),
+                          btnIncOrDec(icon: Assets.icons.removeBlack, isInc: false),
                           Container(
                             width: 20,
                             alignment: Alignment.center,
@@ -264,8 +247,7 @@ class _ItemWidgetState extends State<ItemWidget> {
                   padding: const EdgeInsets.only(bottom: 6.0),
                   child: Text(
                     '${widget.model.subTitle}',
-                    style: AppTextStyles.b4Regular
-                        .copyWith(color: AppColors.metalColor.shade50),
+                    style: AppTextStyles.b4Regular.copyWith(color: AppColors.metalColor.shade50),
                   ),
                 )
               : Container(),
@@ -285,10 +267,7 @@ class _ItemWidgetState extends State<ItemWidget> {
       },
       onTapUp: (TapUpDetails details) => timer?.cancel(),
       onTapCancel: () => timer?.cancel(),
-      child: Container(
-          height: 20,
-          color: const Color(0xffE5E7EB),
-          child: SvgPicture.asset(icon)),
+      child: Container(height: 20, color: const Color(0xffE5E7EB), child: SvgPicture.asset(icon)),
     );
   }
 
